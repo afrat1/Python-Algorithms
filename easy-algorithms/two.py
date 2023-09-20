983ac56fb01ebd843f31c24f6c5c9da383626a70
Ali Firat Kaya <afkaya48300@gmail.com>
2023-09-20 14:53:52 +0300

    two

--- easy-algorithms/two.py
+++ easy-algorithms/two.py
@@ -1,2 +1,11 @@
 # Write a Python program to return a string consisting of the first 3 and the last 3 characters of a string you received from the user.
 # If string length is less than 3, return "empty string".
+
+a = input("Enter your string: ")
+
+if len(a) < 3:
+    print("empty string")
+else:
+    print(a[:3] + a[len(a)-3 : len(a)])
+
+# a[len(a)-3 : len(a)] == a[-3:0]
