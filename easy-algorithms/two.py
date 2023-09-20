52da297eeadc832b4b86d54716934a4b1e90c089
Ali Firat Kaya <afkaya48300@gmail.com>
2023-09-20 14:26:56 +0300

    comment

---
+++ easy-algorithms/two.py
@@ -0,0 +1,2 @@
+# Write a Python program to return a string consisting of the first 3 and the last 3 characters of a string you received from the user.
+# If string length is less than 3, return "empty string".
