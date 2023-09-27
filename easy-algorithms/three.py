e5a5c7b7346a54f470c4dabc0ef19e9bc7a9c646
Ali Firat Kaya <afkaya48300@gmail.com>
2023-09-27 15:39:12 +0300

    three

---
+++ easy-algorithms/three.py
@@ -0,0 +1,9 @@
+# Write a program to find how many times substring "Mr.Bean" appears in the given string.
+
+str_x = "Mr.Bean is good actor and Mr.Bean is very funny."
+
+substring = "Mr.Bean"
+
+count = str_x.count(substring)
+
+print(count)
