11f99c73422c638df4ab1217449dd7d8834f75b7
Ali Firat Kaya <afkaya48300@gmail.com>
2023-10-07 08:51:38 +0300

    eight

--- easy-algorithms/eight.py
+++ easy-algorithms/eight.py
@@ -1,10 +1,19 @@
-# Write a program that takes a country's name and its area as arguments and returns the are of the country's proportion of the total world's landmass.
+# Create an algorithm that finds BMI
 
-country = input("Enter country name: ")
-area = input("Enter the are of the country: ")
+w = int(input("Please enter your weight in kg: "))
+h = int(input("Please enter your height in cm: "))
 
-earth = 148940000
+result = round(w / ((h/100) ** 2),2)
+print(result)
 
-yuzde = round((int(area) / earth) * 100,2)
-
-print(f"{country} is {yuzde} % of the total world' landmass.")
+if result < 18.5:
+    body = "underweight"
+elif result < 25:
+    body = "normal"
+elif result < 30:
+    body = "overweight"
+elif result < 35:
+    body = "obese"
+else:
+    body = "extremely obese"
+print(f"Your bmi is: {result} and your body type is: {body}") 
